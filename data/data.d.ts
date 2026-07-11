a7807294b488873c8f6dc248c2f72313aa499cce
amitprasad02 <amiit.prasad84@gmail.com>
2026-07-11 20:13:38 +0530

    commit

---
+++ data/data.d.ts
@@ -0,0 +1,10 @@
+export interface ARDataItem {
+  id: string;
+  name: string;
+  targetIndex: number;
+  image: string;
+  video: string;
+}
+
+declare const arList: ARDataItem[];
+export default arList;
