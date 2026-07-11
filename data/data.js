01bd0c31f7fcdb52d83868acb797a3f457c22b39
amitprasad02 <amiit.prasad84@gmail.com>
2026-07-11 20:18:04 +0530

    commit

--- data/data.js
+++ data/data.js
@@ -3,15 +3,15 @@ const arList = [
         "id": "target1",
         "name": "Target 1",
         "targetIndex": 0,
-        "image": "./uploads/image/target1768745901172.mind",
-        "video": "./uploads/video/video0.mp4"
+        "image": "uploads/image/target1768745901172.mind",
+        "video": "uploads/video/video0.mp4"
     },
     {
         "id": "target2",
         "name": "Target 2",
         "targetIndex": 0,
-        "image": "./uploads/image/target1768746017399.mind",
-        "video": "./uploads/video/video1.mp4"
+        "image": "uploads/image/target1768746017399.mind",
+        "video": "uploads/video/video1.mp4"
     }
 ];
 
