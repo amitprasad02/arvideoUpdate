a0a5755ce0ecd188095ba426ab27dccc282747ac
amitprasad02 <amiit.prasad84@gmail.com>
2026-07-12 14:52:31 +0530

    commit

---
+++ data/data.js
@@ -0,0 +1,18 @@
+const arList = [
+    {
+        "id": "target1",
+        "name": "Target 1",
+        "targetIndex": 0,
+        "image": "../uploads/image/target1768745901172.mind",
+        "video": "../uploads/video/video0.mp4"
+    },
+    {
+        "id": "target2",
+        "name": "Target 2",
+        "targetIndex": 0,
+        "image": "../uploads/image/target1768746017399.mind",
+        "video": "../uploads/video/video1.mp4"
+    }
+];
+
+export default arList;
